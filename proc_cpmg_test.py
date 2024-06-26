eb821ae7b4155d29d69d4d82a6fe998734646792
Alexandria Guinness <aguinnes@syr.edu>
2024-06-26 10:34:27 -0400

    CPMG testing

---
+++ proc_cpmg_test.py
@@ -0,0 +1,135 @@
+from pylab import *
+from pyspecdata import *
+from pyspecProcScripts import *
+from sympy import exp as s_exp
+fl = figlist_var()
+# {{{ load the files that we're interested in as different nddata objects
+all_data = {}
+pathways = {}
+# {{{ 240624 - 0 dB
+#all_data["Hahn 0 dB"] = find_file(
+#    "240625_200uM_TEMPOL_0dB_echo.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="echo_2",
+#    lookup = lookup_table
+#)
+#all_data["SE 0dB - 1"] = find_file(
+#    "240625_200uM_TEMPOL_0dB_generic_echo.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="echo_2",
+#    lookup = lookup_table
+#)
+#all_data["SE 0dB - 2"] = find_file(
+#    "240625_200uM_TEMPOL_0dB_generic_echo.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="echo_4",
+#    lookup = lookup_table
+#)
+#all_data["CPMG"] = find_file(
+#    "240625_200uM_TEMPOL_prep_generic_CPMG.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="CPMG_1",
+#    lookup = lookup_table
+#)
+#all_data["CPMG 0 dB - 1"] = find_file(
+#    "240625_200uM_TEMPOL_0dB_generic_CPMG.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="CPMG_1",
+#    lookup = lookup_table
+#)
+#all_data["CPMG 0 dB - 1"] = find_file(
+#    "240625_200uM_TEMPOL_0dB_generic_CPMG.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="CPMG_3",
+#    lookup = lookup_table
+#)
+#all_data["CPMG 0 dB - 2"] = find_file(
+#    "240625_200uM_TEMPOL_0dB_generic_CPMG.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="CPMG_5",
+#    lookup = lookup_table
+#)
+# }}}
+# {{{ 240624 - 30 dB
+all_data["CPMG 30 dB - 1"] = find_file(
+    "240625_200uM_TEMPOL_30dB_generic_CPMG.h5",
+    exp_type="ODNP_NMR_comp/Echoes",
+    expno="CPMG_1",
+    lookup = lookup_table
+)
+all_data["CPMG 30 dB - 2"] = find_file(
+    "240625_200uM_TEMPOL_30dB_generic_CPMG.h5",
+    exp_type="ODNP_NMR_comp/Echoes",
+    expno="CPMG_3",
+    lookup = lookup_table
+)
+all_data["SE 30 dB - 1"] = find_file(
+    "240625_200uM_TEMPOL_30dB_generic_echo.h5",
+    exp_type="ODNP_NMR_comp/Echoes",
+    expno="echo_2",
+    lookup = lookup_table
+)
+all_data["SE 30 dB - 2"] = find_file(
+    "240625_200uM_TEMPOL_30dB_generic_echo.h5",
+    exp_type="ODNP_NMR_comp/Echoes",
+    expno="echo_4",
+    lookup = lookup_table
+)
+#all_data["SE 34 dB - 1"] = find_file(
+#    "240625_200uM_TEMPOL_34dB_generic_echo.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="echo_2",
+#    lookup = lookup_table
+#)
+#all_data["SE 34 dB - 2"] = find_file(
+#    "240625_200uM_TEMPOL_34dB_generic_echo.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="echo_4",
+#    lookup = lookup_table
+#)
+#all_data["CPMG 34 dB - 1"] = find_file(
+#    "240625_200uM_TEMPOL_34dB_generic_CPMG.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="CPMG_1",
+#    lookup = lookup_table
+#)
+#all_data["CPMG 34 dB - 2"] = find_file(
+#    "240625_200uM_TEMPOL_34dB_generic_CPMG.h5",
+#    exp_type="ODNP_NMR_comp/Echoes",
+#    expno="CPMG_3",
+#    lookup = lookup_table
+#)
+# }}}
+with figlist_var() as fl:
+    for thislabel, thisd in all_data.items():
+        obs(
+                f"for {thislabel} spectral width {thisd.get_prop('acq_params')['SW_kHz']}~kHz"
+                )
+        if 'nScans' in thisd.dimlabels:
+            thisd *= thisd.shape['nScans']
+            thisd.mean('nScans')
+        d = thisd
+        if 'Hahn' in thislabel:
+            d /= sqrt(d.shape['ph1'])
+        else:
+            d /= sqrt(d.shape['ph_overall']*d.shape['ph1'])
+        d.ift('t2')
+        fl.next('raw data for %s'%thislabel)
+        #if 'indirect' in d.dimlabels:
+        #    d.rename('indirect','nEcho')
+        #    d.reorder(['ph_overall','ph_diff','nEcho','t2'])
+        fl.image(d)
+        d.ft('t2')
+        fl.next('SE vs CPMG[nEcho,0] - 30 dBm')
+        d.ift('t2')
+        if 'CPMG' in thislabel:
+            d = d['nEcho',:3]
+            d.smoosh(['nEcho','t2'],'t2')
+            acq = d.get_prop('acq_params')
+            echo_time = 1e-6*2*(acq['tau_us']+acq['p90_us'])
+            d['t2'] = (d['t2']['nEcho']) * echo_time + d['t2']['t2']
+            d = select_pathway(d,d.get_prop('coherence_pathway'))
+            fl.plot(abs(d),'.',label = thislabel)
+        else:
+            fl.plot(abs(select_pathway(d['t2':(0,15e-3)],d.get_prop('coherence_pathway'))),'.',label = thislabel)
+
--- run_nutation_actual_p90.py
+++ run_nutation_actual_p90.py
@@ -2,40 +2,42 @@ from pylab import *
 from pyspecdata import *
 import os
 import SpinCore_pp
-import socket
-import sys
-import time
-from datetime import datetime
+from SpinCore_pp import get_integer_sampling_intervals
 from SpinCore_pp.ppg import run_spin_echo
+from datetime import datetime
 from SpinCore_pp import prog_plen
+import h5py
 import logging
 fl = figlist_var()
-#{{{Parameters that change for new samples
-output_name = '200uM_TEMPOL_nutation_1'
-adcOffset = 63
-carrierFreq_MHz = 14.89333634
-nScans = 4
-nEchoes = 1
-repetition = 13000000.0
-p90_range = linspace(1,6,5,endpoint=False)
-SW_kHz = 3.9 #24.0 originally
-acq_time = 1024.
-tau_us = 5000
-#}}}
-ph1_cyc = r_[0,1,2,3]
-#{{{These should stay the same regardless of sample
+p90_range = linspace(1,6,6,endpoint=False)
+# {{{importing acquisition parameters
+config_dict = SpinCore_pp.configuration("active.ini")
+nPoints, config_dict['SW_kHz'], config_dict['acq_time_ms'] = get_integer_sampling_intervals(config_dict['SW_kHz'], config_dict['acq_time_ms'])
+my_exp_type = "ODNP_NMR_comp/nutation"
+target_directory = getDATADIR(exp_type=my_exp_type)
+assert os.path.exists(target_directory)
+# }}}
+# {{{create filename and save to config file
 date = datetime.now().strftime('%y%m%d')
-# NOTE: Number of segments is nEchoes * nPhaseSteps
-nPoints = int(acq_time*SW_kHz+0.5)
-acq_time = nPoints/SW_kHz # ms
-logging.debug("ACQUISITION TIME:",acq_time,"ms")
-logging.debug("TAU DELAY:",tau_us,"us")
-#}}}
-# {{{ check for file
-myfilename = date + "_" + output_name + ".h5"
-if os.path.exists(myfilename):
-    raise ValueError(
-        "the file %s already exists, so I'm not going to let you proceed!" % myfilename
+config_dict['type'] = 'nutation'
+config_dict['date'] = date
+config_dict["echo_counter"] += 1
+filename = (
+    f"{config_dict['date']}_{config_dict['chemical']}_{config_dict['type']}"
+)
+# }}}
+# {{{set phase cycling
+ph1_cyc = r_[0, 1, 2, 3]
+nPhaseSteps = 4
+# }}}
+# {{{check total points
+total_pts = nPoints * nPhaseSteps
+assert total_pts < 2**14, (
+    "You are trying to acquire %d points (too many points) -- either change SW or acq time so nPoints x nPhaseSteps is less than 16384\nyou could try reducing the acq_time_ms to %f"
+    % (total_pts, config_dict["acq_time_ms"] * 16384 / total_pts)
+)
+# }}}
+
     )
 # }}}
 prog_p90s = []
@@ -43,19 +45,18 @@ for j in range(len(p90_range)):
     prog_p90_us = prog_plen(p90_range[j])
     prog_p180_us = prog_plen(2 * p90_range[j])
     prog_p90s.append(prog_p90_us)
-print(prog_p90s)    
 nutation_data = run_spin_echo(
         deadtime_us = 20.0,
-        nScans=nScans, 
+        nScans=config_dict['nScans'], 
         indirect_idx = 0, 
         indirect_len = len(p90_range), 
-        adcOffset = adcOffset,
-        carrierFreq_MHz = carrierFreq_MHz, 
+        adcOffset = config_dict['adcOffset'],
+        carrierFreq_MHz = config_dict['carrierFreq_MHz'], 
         nPoints = nPoints,
-        nEchoes=nEchoes, 
+        nEchoes=config_dict['nEchoes'], 
         p90_us = p90_range[0], 
-        repetition_us = repetition,
-        tau_us = tau_us, 
+        repetition_us = config_dict['repetition_us'],
+        tau_us = config_dict['tau_us'], 
         SW_kHz = SW_kHz, 
         indirect_fields = None, 
         ret_data = None)
@@ -63,39 +64,72 @@ nutation_times = nutation_data.getaxis('indirect')
 nutation_times[0] = p90_range[0]
 for index,p90 in enumerate(p90_range[1:]):
     run_spin_echo(
-            nScans=nScans, 
+            nScans=config_dict['nScans'], 
             deadtime_us = 20.0,
             indirect_idx = index+1, 
             indirect_len = len(p90_range), 
-            adcOffset = adcOffset,
-            carrierFreq_MHz = carrierFreq_MHz, 
+            adcOffset = config_dict['adcOffset'],
+            carrierFreq_MHz = config_dict['carrierFreq_MHz'], 
             nPoints = nPoints,
-            nEchoes=nEchoes, 
+            nEchoes=config_dict['nEchoes'], 
             p90_us = p90, 
-            repetition_us = repetition,
-            tau_us = tau_us, 
+            repetition_us = config_dict['repetition_us'],
+            tau_us = config_dict['tau_us'], 
             SW_kHz = SW_kHz, 
             ret_data = nutation_data)
     nutation_times[index + 1] = p90
-acq_params = {j:eval(j) for j in dir() if j in ['adcOffset', 'carrierFreq_MHz', 'amplitude',
-    'nScans', 'nEchoes', 'p90_range', 'prog_p90s','deadtime', 'repetition', 'SW_kHz',
-    'nPoints', 'deblank_us', 'tau_us', 'nPhaseSteps']}
-acq_params['pulprog'] = 'spincore_nutation_v3'
-nutation_data.set_prop('acq_params',acq_params)
-nutation_data.name('nutation')
-myfilename = date + '_' + output_name + '.h5'
-nutation_data.chunk('t',['ph1','t2'],[len(ph1_cyc),-1]).setaxis('ph1',ph1_cyc/4)
-nutation_data.setaxis('nScans',r_[0:nScans])        
-nutation_data.reorder('t2',first=False)
-nutation_data.hdf5_write(myfilename)
-logging.info("Name of saved data",nutation_data.name())
-logging.info("Shape of saved data",ndshape(nutation_data))
-SpinCore_pp.stopBoard()
-nutation_data.reorder(['ph1','indirect'])
-fl.next('raw data')
-fl.image(nutation_data.C.setaxis('indirect','#').set_units('indirect','scan #'))
-nutation_data.ft('t2',shift=True)
-fl.next('FT raw data')
-fl.image(nutation_data.C.setaxis('indirect','#').set_units('indirect','scan #'))
+# {{{ chunk and save data
+nutation_data.set_prop("postproc_type", "spincore_nutation_v3")
+nutation_data.set_prop("coherence_pathway", {"ph1": +1})
+nutation_data.set_prop("acq_params", config_dict.asdict())
+nutation_data.name(config_dict["type"] + "_" + str(config_dict["echo_counter"]))
+nutation_data.chunk(
+        "t", 
+        ["ph1", "t2"], 
+        [4, -1]
+)
+nutation_data.labels(
+    {
+        "ph1": r_[0 : len(ph1_cyc)]
+    }
+)
+nutation_data.setaxis("ph1", ph1_cyc / 4)
+if config_dict["nScans"] > 1:
+    nutation_data.setaxis("nScans", r_[0 : config_dict["nScans"]])
+nutation_data.reorder(["ph1", "nScans", "t2"])
+filename_out = filename + ".h5"
+nodename = nutation_data.name()
+if os.path.exists(f"{filename_out}"):
+    print("this file already exists so we will add a node to it!")
+    with h5py.File(
+        os.path.normpath(os.path.join(target_directory, f"{filename_out}"))
+    ) as fp:
+        if nodename in fp.keys():
+            print("this nodename already exists, so I will call it temp_nutation")
+            nutation_data.name("temp_nutation")
+            nodename = "temp_nutation"
+    nutation_data.hdf5_write(f"{filename_out}", directory=target_directory)
+else:
+    try:
+        nutation_data.hdf5_write(f"{filename_out}", directory=target_directory)
+    except:
+        print(
+            f"I had problems writing to the correct file {filename}.h5, so I'm going to try to save your file to temp_nutation.h5 in the current directory"
+        )
+        if os.path.exists("temp_nutation.h5"):
+            print("there is a temp_nutation.h5 already! -- I'm removing it")
+            os.remove("temp_nutation.h5")
+        nutation_data.hdf5_write("temp_nutation.h5")
+        print(
+            "if I got this far, that probably worked -- be sure to move/rename temp_nutation.h5 to the correct name!!"
+        )
+print("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
+print("saved data to (node, file, exp_type):", nutation_data.name(), filename_out, my_exp_type)
+config_dict.write()
+nutation_data.ft("t2", shift=True)
+fl.next("image - ft")
+fl.image(nutation_data)
+fl.next("image - ft, coherence")
+nutation_data.ft("ph1")
+fl.image(nutation_data)
 fl.show()
-
